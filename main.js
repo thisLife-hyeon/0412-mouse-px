54599547c3e96ee0cb011949482c937a169fada0
thislife-hyeon <90155180+thisLife-hyeon@users.noreply.github.com>
2022-04-19 15:22:57 +0900

    curX curY create

--- main.js
+++ main.js
@@ -5,18 +5,34 @@ const tag = document.querySelector(".tag");
 
 addEventListener("load", function () {
   const targetRect = target.getBoundingClientRect();
+
   const targetHalfWidth = targetRect.width / 2;
   const targetHalfHeight = targetRect.height / 2;
 
-  document.addEventListener("mousemove", (event) => {
-    const x = event.clientX;
-    const y = event.clientY;
-    // console.log(x, y);
+  let curX = 0;
+  let curY = 0;
+  let mouseX = 0;
+  let mouseY = 0;
 
-    vertical.style.transform = `translateX(${x}px)`;
-    horizontal.style.transform = `translateY(${y}px)`;
-    target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
-    tag.style.transform = `translate(${x}px, ${y}px)`;
-    tag.innerHTML = `${x}px ${y}px`;
+  document.addEventListener("mousemove", (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    // const x = event.clientX;
+    // const y = event.clientY;
+    console.log(x, y);
   });
+
+  function animate() {
+    requestAnimationFrame(animate);
+    curX += (mouseX - curX) * 0.2;
+    curY += (mouseY - curY) * 0.2;
+
+    vertical.style.transform = `translateX(${curX}px)`;
+    horizontal.style.transform = `translateY(${curY}px)`;
+    target.style.transform = `translate(${curX - targetHalfWidth}px, ${curY - targetHalfHeight}px)`;
+    tag.style.transform = `translate(${curX}px, ${curY}px)`;
+    tag.innerHTML = `${Math.ceil(curX)}px ${Math.ceil(curY)}px`;
+  }
+
+  animate();
 });
